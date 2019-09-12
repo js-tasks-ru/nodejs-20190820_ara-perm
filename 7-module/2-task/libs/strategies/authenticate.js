2e769b118b2a35da29a03c06cc34a1d975407587
Andrey Reutov <reutov.andrey@gmail.com>
2019-09-12 18:05:20 +0300

    temp save 7-2

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,34 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  //try {
+    if (!email) {done(null, false, `Не указан email`); return;}
+
+    const tmpUser = {
+      email,
+      displayName
+    };
+    
+    let user = await User.find({email});
+   
+    if (user) {
+       done (null, user);
+    } else
+    { 
+      user = new User({
+        email, 
+        displayName
+      });
+      await user.save();
+    }
+    /*const user = await User.findOneAndUpdate({email}, tmpUser, {
+      new: true,
+      upsert: true
+    });*/
+    //done(null, user);
+ // } catch (e) {
+    //done(e)
+  //}
+
+   //done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
 };
