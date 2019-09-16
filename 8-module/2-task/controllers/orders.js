81450df1b02d294a94de6030a0c9f19868acca3e
Reutov Andrey <reutov.andrey@gmail.com>
2019-09-16 22:13:14 +0300

    module-8 2-task temp save

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -2,6 +2,17 @@ const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    const {phone, address, product} = ctx.request.body;
+    //ctx.user
+    const order = new Order({user: ctx.user.id, product, phone, address});
+    await order.save();
+    await sendMail({
+        template: 'order-confirmation',
+        locals: {token: verificationToken},
+        to: ctx.user.email,
+        subject: 'Подтвердите почту',
+    });
+
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
