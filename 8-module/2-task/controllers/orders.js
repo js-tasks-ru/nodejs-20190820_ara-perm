3580215b63ac42b0fa8558d04ee3cd64b2e602cf
Andrey Reutov <reutov.andrey@gmail.com>
2019-09-17 11:28:12 +0300

    a few corrections to 8-2

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,35 +1,29 @@
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
 
 module.exports.checkout = async function checkout(ctx, next) {
-    if (ctx.user) {
+    
     const {phone, address, product} = ctx.request.body;
     const order = new Order({user: ctx.user.id, product, phone, address});
     await order.populate('product').execPopulate();
     await order.save();
     await sendMail({
         template: 'order-confirmation',
-        locals: {id: order.product.id,
-                product},
+        locals: {
+                    id:      order.product.id,
+                    product: {title: order.product.title}
+                },
         to: ctx.user.email,
-        subject: 'Подтвердите почту',
+        subject: 'Подтверждение создания заказа',
     });
     ctx.body = {order: order.id}
-    } else {
-        ctx.status = 401;
-    }
     return next();
 
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
-    if (ctx.user) {
-        const arr = await Order.find({user: ctx.user.id}).populate('product');
-        ctx.body = {orders: arr};
-    } else
-    {
-        ctx.status = 401;
-    }
+    const orders = await Order.find({user: ctx.user.id}).populate('product');
+    ctx.body = {orders: orders.map(mapOrder)};
     return next();
-
 };
